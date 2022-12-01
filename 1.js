55f686ae13097f4231a7ba7b0705c2b7f0ea3400
Shubham Tidke <shubhamtidke0199@gmail.com>
2022-12-01 23:37:38 +0530

    new object unmutated

--- 1.js
+++ 1.js
@@ -38,3 +38,42 @@ const students = [
   }
   
   console.log(convertArrayToObject(students))
+
+// 3.Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+
+const student1 = {
+    name: 'David',
+    age: 25,
+    skills: {
+        frontEnd: [
+            { skill: 'HTML', level: 10 },
+            { skill: 'CSS', level: 8 },
+            { skill: 'JS', level: 8 },
+            { skill: 'React', level: 9 }
+        ],
+        backEnd: [
+            { skill: 'Node',level: 7 },
+            { skill: 'GraphQL', level: 8 },
+        ],
+        dataBase:[
+            { skill: 'MongoDB', level: 7.5 },
+        ],
+        dataScience:['Python', 'R', 'D3.js']
+    }
+}
+
+
+let newObject = {...student1}
+// i.Add Bootstrap with level 8 to the front end skill sets 
+newObject.skills.frontEnd.push({ skill: 'Bootstrap', level: 8 })
+
+//ii. Add Express with level 9 to the back end skill sets
+newObject.skills.backEnd.push({ skill: 'Express', level: 9 })
+
+// iii. Add SQL with level 8 to the data base skill sets
+newObject.skills.dataBase.push({ skill: 'SQL', level: 8 })
+
+//iv.Add SQL without level to the data science skill sets
+newObject.skills.dataScience.push('SQL')
+
+console.log(newObject)
